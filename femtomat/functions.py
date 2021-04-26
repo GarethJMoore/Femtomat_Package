8b7278e8b521477c316dba057985adf68c76ef25
Gareth Moore <54354117+GarethJMoore@users.noreply.github.com>
2021-04-26 09:58:43 +0200

    version 0.1.0

--- femtomat/functions.py
+++ femtomat/functions.py
@@ -1,6 +1,14 @@
 import numpy as np
 
 def nearest(array, value): 
+    '''
+    Returns index of element in array with value closest to the input value.
+
+    :param array: Input array in which to find index.
+    :param value: Value to search for in array.
+
+    :return index: Returns index of element in array with value closest to the input value.
+    '''
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
     return idx
@@ -8,6 +16,14 @@ def nearest(array, value):
 
 #noise filtering functions for TA
 def svd_reconstruction_E15(Data,k):
+    '''
+    Function which reconstructs a data matrix from eigenvectors and eigenvalues (determined by SVD) by only using the top :math:`k` eigenvalues and eigenvectors.
+
+    :param Data: Data matrix which is decomposed and reconstructed.
+    :param k: The number of eigenvectors and eigenvalues used to reconstruct the data matrix.
+
+    :returns rec: Reconstructed data matrix.
+    '''
     D = Data.shape[0]
     T = Data.shape[1]
     u, s, vh = np.linalg.svd(Data, full_matrices=False)
@@ -17,6 +33,14 @@ def svd_reconstruction_E15(Data,k):
     return rec
 
 def rmse_special(Data, rec):
+    '''
+    A  RSME calculator to calculate the Root Mean Square Error of the original data matrix and the reconstructed data matrix with a threshold of the :math:`\\sqrt{2/D}`. This is used to determine :math:`k`.
+
+    :param Data: Original data matrix.
+    :param rec: Reconstructed data matrix.
+    
+    :returns rmse: Returns either :math:`\\sqrt{2/D}` if the RMSE is greater than :math:`\\sqrt{2/D}` or the RMSE.
+    '''
     D = Data.shape[0]
     temp = np.sqrt(np.sum((Data-rec)**2)/D)
     if temp >= np.sqrt(2/D):
@@ -26,10 +50,11 @@ def rmse_special(Data, rec):
 
 def k_opt(Data):
     '''
-    Function returning the optimal number of eigen vectors and values for reconstruction (k)
-    ----------------------------------------------------------------------------------------
-    input:  Data
-    output: k
+    Function returning the optimal number of eigenvectors and eigenvalues for reconstructing the a data matrix (:math:`k`).
+    
+    :param Data: Data matrix to find :math:`k` for.
+
+    :returns :math:`k`: Optimal number of eigenvectors and eigenvalues for reconstructing the a data matrix.
     '''
     D = Data.shape[0]
     T = Data.shape[1]
